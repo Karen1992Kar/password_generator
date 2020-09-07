57bf300c1be65784844e548697b9a2c36f0027c8
John Smith <you@example.com>
2020-09-07 18:04:57 +0400

    ստեղծեցի նոր ֆունկցիա test_for_string_argument անունով, որը ստուգում է password մոդուլի աշխատանքը ոչ բնական թվերի համար

--- test_password.py
+++ test_password.py
@@ -8,8 +8,14 @@ import string
 import pytest
 from password import password_generator
 
+# TODO: use string module's property/functions
+lowers = string.ascii_lowercase
+uppers = string.ascii_uppercase
+spec_simbols = string.punctuation
+ints = string.digits
 
-@pytest.mark.parametrize("num", [1,2,3,4,5,6,7])
+
+@pytest.mark.parametrize("num", [1,4,7])
 def test_argument_greater_than_seven(num):
     """ֆունկցիան ստուգում է password մոդուլը 8 ից փոքր բնական թվերի համար"""
 
@@ -22,14 +28,55 @@ def test_argument_greater_than_seven(num):
                     "աշխատեց նորմալ".format(num))
 
 
-@pytest.mark.parametrize('len_password', [8,9,25,30])
-@pytest.mark.parametrize('type_simbols', [string.punctuation,\
-                          str(list(range(0, 10))), string.ascii_letters[:26],\
-                          string.ascii_letters[26:]])
-def test_str_in_password(len_password, type_simbols):
-    """Ֆունկցիան ստուգում է գախտնաբառի մեջ հատուկ սիմվոլների ի առկայուտյունը """
+@pytest.mark.parametrize('len_password', [8, 9, 35, 36])
+def test_str_in_password(len_password):
+    """Ֆունկցիան ստուգում է գախտնաբառի մեջ հատուկ սիմվոլների, տվերի, " \
+    մեծատառերի, և փոքրատառերի առկայուտյունը """
 
-    assert set(password_generator(len_password)).intersection(type_simbols), "գաղտնաբառը "\
-           "պետք է բարունակեր {} սիմվոլներից մեկը, բայց չի " \
-           "բարունակում".format(type_simbols)
+    password = password_generator(len_password)
+
+    assert set(password).intersection(lowers), "գաղտնաբառը "\
+        "պետք է բարունակեր {} սիմվոլներից մեկը, բայց չի " \
+        "բարունակում".format(lowers)
+
+    assert set(password).intersection(uppers), "գաղտնաբառը "\
+        "պետք է բարունակեր {} սիմվոլներից մեկը, բայց չի " \
+        "բարունակում".format(uppers)
+
+    assert set(password).intersection(spec_simbols), "գաղտնաբառը "\
+        "պետք է բարունակեր {} սիմվոլներից մեկը, բայց չի " \
+        "բարունակում".format(spec_simbols)
+
+    assert set(password).intersection(ints), "գաղտնաբառը "\
+        "պետք է բարունակեր {} սիմվոլներից մեկը, բայց չի " \
+        "բարունակում".format(ints)
+
+
+@pytest.mark.parametrize("argument", ["string", "16"])
+def test_for_string_argument(argument):
+    """Ֆունկցիան ստուգում է password մոդուլի աշխատանքը ոչ
+    բնական թվերի դեպքում"""
+
+    try:
+        password_generator(argument)
+    except TypeError as desc_name:
+        assert str(desc_name) == "Ֆունկցիան աշխատում է բնական թվի համար. "\
+               "'{}'-ը չի համարվում բնական թիվ".format(argument)
+    else:
+        pytest.fail("ֆունկցիան պետք է կանչեր TypeError <<{}>> արգումենտի "
+                    "համար".format(argument))
+
+
+def test_for_len_arguments():
+    
+
+
+
+
+
+
+
+
+
+
 
