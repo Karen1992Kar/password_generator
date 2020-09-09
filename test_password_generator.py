07bdf6a2a74a63bc2e31e332298f3f20e6ecb8c1
John Smith <you@example.com>
2020-09-09 15:58:52 +0400

    test_password_generator մոդուլում ստեղծվեց test_for_check_random_password ֆունկցիան, որը ստուգում է առանձին սիմվոլների հավանականությունը

--- test_password_generator.py
+++ test_password_generator.py
@@ -6,23 +6,43 @@
 
 import string
 import pytest
-from password_generator import generator_password
+from password_generator import generate_password, MIN_LENGTH_PASSWORD
 
-# TODO: use string module's property/functions
+
 LOWERS = string.ascii_lowercase
 UPPERS = string.ascii_uppercase
-SPEC_SIMBOLS = string.punctuation
-INTS = string.digits
+SPEC_SYMBOLS = string.punctuation
+INTEGERS = string.digits
 
 
-@pytest.mark.parametrize("num", [1,4,7])
+def interest_calculator(count_type, number_of_cycles, length_password):
+    """Ֆունկցիան վերադարձնում է առանձին սիմվոլների քանակը գաղտնաբառի մեջ
+    ըստ տոկոսների """
+
+    return (count_type * 100)/(number_of_cycles*length_password)
+
+
+def counter_of_identical_symbols(string1, string2):
+    """Ֆունկցիան վերադարձնում է string1 ի մեջ եղած են սիմվոլների քանակը, որոնք
+    կան string2 ի մեջ"""
+
+    result = []
+    for i in string2:
+        if i in string1:
+            result.append(i)
+
+    return len(result)
+
+
+@pytest.mark.parametrize("num", [1, 4, 7])
 def test_argument_greater_than_seven(num):
-    """ֆունկցիան ստուգում է password մոդուլը 8 ից փոքր բնական թվերի համար"""
+    """Ֆունկցիան ստուգում է password մոդուլը 8 ից փոքր բնական թվերի համար"""
 
     try:
-        generator_password(num)
+        generate_password(num)
     except ValueError as desc_name:
-        assert str(desc_name) == "ֆունկցիաին փոխանցած թիվը պետք է մեծ լինի 7 ից"
+        assert str(desc_name) == "ֆունկցիաին փոխանցած թիվը պետք է " \
+                                 "մեծ լինի {} ից".format(MIN_LENGTH_PASSWORD)
     else:
         pytest.fail("Սպասվում էր ValueError {} թվի համար, բայց ֆունկցիան "
                     "աշխատեց նորմալ".format(num))
@@ -33,7 +53,7 @@ def test_str_in_password(len_password):
     """Ֆունկցիան ստուգում է գախտնաբառի մեջ հատուկ սիմվոլների, թվերի, " \
     մեծատառերի, և փոքրատառերի առկայուտյունը """
 
-    password = generator_password(len_password)
+    password = generate_password(len_password)
 
     assert set(password).intersection(LOWERS), "գաղտնաբառը "\
         "պետք է բարունակեր {} սիմվոլներից մեկը, բայց չի " \
@@ -43,13 +63,13 @@ def test_str_in_password(len_password):
         "պետք է բարունակեր {} սիմվոլներից մեկը, բայց չի " \
         "բարունակում".format(UPPERS)
 
-    assert set(password).intersection(SPEC_SIMBOLS), "գաղտնաբառը "\
+    assert set(password).intersection(SPEC_SYMBOLS), "գաղտնաբառը "\
         "պետք է բարունակեր {} սիմվոլներից մեկը, բայց չի " \
-        "բարունակում".format(SPEC_SIMBOLS)
+        "բարունակում".format(SPEC_SYMBOLS)
 
-    assert set(password).intersection(INTS), "գաղտնաբառը "\
+    assert set(password).intersection(INTEGERS), "գաղտնաբառը "\
         "պետք է բարունակեր {} սիմվոլներից մեկը, բայց չի " \
-        "բարունակում".format(INTS)
+        "բարունակում".format(INTEGERS)
 
 
 @pytest.mark.parametrize("argument", ["string", "16", 7.3, True])
@@ -58,7 +78,7 @@ def test_for_string_argument(argument):
     բնական թվերի դեպքում"""
 
     try:
-        generator_password(argument)
+        generate_password(argument)
     except TypeError as desc_name:
         assert str(desc_name) == "Ֆունկցիան աշխատում է բնական թվի համար. "\
                "'{}'-ը չի համարվում բնական թիվ".format(argument)
@@ -73,5 +93,28 @@ def test_for_len_arguments(number):
     """Ֆունկցիան ստուգում է password_generator մոդուլին փոխանցած թվի և հետ
     վերադարձրած գաղտնաբառի երկարության հավասար լինելը"""
 
-    assert len(generator_password(number)) == number, "ֆունկցիաին փոխանցած "\
+    assert len(generate_password(number)) == number, "ֆունկցիաին փոխանցած "\
            "թիվը պետք է հավասար լիներ գաղտնաբառի երկարությանը"
+
+
+
+
+@pytest.mark.parametrize("symbols", [LOWERS, UPPERS, INTEGERS, SPEC_SYMBOLS])
+def test_for_check_random_password(symbols):
+    """Ֆունկցիան ստուգում է գաղտնաբառի random լինելը"""
+
+    symbol_count = 0
+
+    number_of_cycles = 100
+    length_password = 12
+
+    for _ in range(number_of_cycles):
+
+        password = generate_password(length_password)
+        symbol_count += counter_of_identical_symbols(symbols, password)
+
+    symbol_procent = interest_calculator(symbol_count, number_of_cycles,
+                                         length_password)
+    assert symbol_procent > 20 or symbol_procent < 30, "Փոքրատառերի քանակը "\
+           "պետք է կազմեր ընդհանուր սիմվոլների քանակի 20 ից 30 տոկոսը, բայց "\
+           "կազմեց {} տոկոս".format(symbol_procent)
