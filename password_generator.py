b8fbb38af8b55ac9bca5dc537bfcc4ebed33980d
John Smith <you@example.com>
2020-09-09 15:24:34 +0400

    Performed the 3rd review, removed several old TODO-s and added a new one.

--- password_generator.py
+++ password_generator.py
@@ -9,13 +9,14 @@ import random
 import argparse
 import string
 
-PUNCTUATION = string.punctuation
+SPEC_SYMBOLS = string.punctuation
 LOWERS = string.ascii_lowercase
 UPPERS = string.ascii_uppercase
-INTS = string.digits
+INTEGERS = string.digits
+
+MIN_LENGTH_PASSWORD = 8
 
 
-#TODO: Move this function up
 def parse_input_argument():
     """Ֆունկցիան ստուգում է հրամանին ուղարկված արգումենտները"""
 
@@ -30,52 +31,53 @@ def check_natural_number(argument):
     """Ֆունկցիան ստուգում է արգումենտի բնական թիվ լինելը."""
 
     if not isinstance(argument, int) or isinstance(argument, bool) or \
-        argument < 1 and argument != 0:
+       argument < 1:
         raise TypeError("Ֆունկցիան աշխատում է բնական թվի համար. "
                         "'{}'-ը չի համարվում բնական թիվ".format(argument))
 
 
 # TODO: Rename function
-def check_size_number(number):
+def check_min_length_password(number):
     # TODO: Improve docstring and provide more generic information
-    "ֆունկցիան ստուգում է իրեն փոխանցած արժեքը"
+    """Գաղտնաբառի մինիմալ երկարության ստուգում"""
 
-    len_pass = 8
+    if number < MIN_LENGTH_PASSWORD:
 
-    if number < len_pass:
         raise ValueError("ֆունկցիաին փոխանցած թիվը պետք է մեծ լինի {} "
-                        "ից".format(len_pass))
+                         "ից".format(MIN_LENGTH_PASSWORD))
 
 
-def generator_random_simbols(total):
-    """ֆունկցիան վերցնում է բնական թիվ և այդ թվին համապատասխան երկարության հետ
+def generate_random_symbols(total):
+    """Ֆունկցիան վերցնում է բնական թիվ և այդ թվին համապատասխան երկարության հետ
     վերադարձնում random սիմվոլներ"""
 
-    all_types = [PUNCTUATION, LOWERS, UPPERS, INTS]
+    all_types = [SPEC_SYMBOLS, LOWERS, UPPERS, INTEGERS]
 
-    rand_simbols = [random.choice(PUNCTUATION), random.choice(LOWERS),
-           random.choice(UPPERS), random.choice(INTS)]
+    # TODO: Correct typos in 'simbol'
+    rand_simbols = [random.choice(SPEC_SYMBOLS), random.choice(LOWERS),
+                    random.choice(UPPERS), random.choice(INTEGERS)]
 
     for _ in range(total - 4):
         index = random.randint(0, 3)
         rand_simbols.append(random.choice(all_types[index]))
 
-    return rand_simbols
+    random.shuffle(rand_simbols)
+    return ''.join(rand_simbols)
 
 
 # TODO: Rename function
-def generator_password(length):
-    """ֆունկցիան ստանում է բնական թիվ, հետ է վերադարձնում գախտանբառ,որի
+def generate_password(length):
+    """Ֆունկցիան ստանում է բնական թիվ, հետ է վերադարձնում գախտանբառ,որի
     երկարությունը համապատասխանում է  այդ թվի չափին"""
 
     check_natural_number(length)
-    check_size_number(length)
-    result = generator_random_simbols(length)
-    # TODO: Use some shuffling function instead of random.sample
-    random.shuffle(result)
-    return ''.join(result)
+    check_min_length_password(length)
+    # TODO: Renamne 'result' variable
+    password = generate_random_symbols(length)
+    # TODO: Move suffle to the function above
+    return password
 
 
 if __name__ == "__main__":
     num = parse_input_argument()
-    print(generator_password(num))
+    print(generate_password(num))
